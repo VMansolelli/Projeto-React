5536ecd975e59ff181a5ca1b0dd624633f1df92f
VMansolelli <victor_vcm@hotmail.com>
2026-09-04 11:39:02 -0300

    feat: adicionar funcionalidade de busca e exibição de contatos na página Contato

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,21 @@
-import express, { type Express, type Request, type Response } from 'express';
+import express, { type Express, type Request, type Response } from "express";
+import cors from "cors";
 
 const app: Express = express();
+app.use(cors());
 const port = 3000;
 
 const contatos = [
-    { id: 1, name: "Mauro", email: "canivete@teste.com"},
-    { id: 2, name: "Samuel", email: "samuelemauro@teste.com"},
-    { id: 3, name: "Gu", email: "passeio@teste.com"}
+  { id: 1, name: "Mauro", email: "canivete@teste.com" },
+  { id: 2, name: "Samuel", email: "samuelemauro@teste.com" },
+  { id: 3, name: "Gu", email: "passeio@teste.com" },
 ];
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
-app.get('/api/contatos', (req: Request, res: Response) => {
+app.get("/api/contatos", (req: Request, res: Response) => {
   res.json(contatos);
 });
 
